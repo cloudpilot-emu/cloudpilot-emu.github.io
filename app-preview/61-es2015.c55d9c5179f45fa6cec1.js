fc0fa03dfdfbbfeb8a42e3834b6d960ad0b54f3e
The Golem <golem@nowhere.org>
2021-03-23 21:20:15 +0000

    Bump.

--- app-preview/61-es2015.c55d9c5179f45fa6cec1.js
+++ app-preview/61-es2015.c55d9c5179f45fa6cec1.js
@@ -1,1 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[61],{oD4e:function(t,n,e){"use strict";e.r(n),e.d(n,"startStatusTap",function(){return c});var o=e("2atR"),s=e("Ke8Y");const c=()=>{const t=window;t.addEventListener("statusTap",()=>{Object(o.h)(()=>{const n=document.elementFromPoint(t.innerWidth/2,t.innerHeight/2);if(!n)return;const e=n.closest("ion-content");e&&new Promise(t=>Object(s.c)(e,t)).then(()=>{Object(o.f)(()=>e.scrollToTop(300))})})})}}}]);
+//# sourceMappingURL=61-es2015.c55d9c5179f45fa6cec1.js.map
