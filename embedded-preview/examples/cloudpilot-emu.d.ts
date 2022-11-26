533c9450cd1ab709da926f9b58a883038daa19d4
The Golem <golem@nowhere.org>
2022-11-26 09:21:38 +0000

    Bump.

--- embedded-preview/examples/cloudpilot-emu.d.ts
+++ embedded-preview/examples/cloudpilot-emu.d.ts
@@ -52,13 +52,13 @@ export interface EmulationStatistics {
 	/**
 	 * The ratio between the duration of an emulated timeslice and the time that
 	 * is required by the host to emulate it. If this drops below one the host
-	 * cannot keep up with the emulation, and Cloudpilot automatically reduces
+	 * cannot keep up with the emulation, and CloudpilotEmu automatically reduces
 	 * emulation speed.
 	 */
 	hostSpeed: number;
 	/**
 	 * The relative speed of the emulated device. One means full speed, lower
-	 * values indicate that Cloudpilot reduced speed in order to compensate for
+	 * values indicate that CloudpilotEmu reduced speed in order to compensate for
 	 * a slow host.
 	 */
 	emulationSpeed: number;
@@ -69,7 +69,7 @@ export interface EmulationStatistics {
 	averageFps: number;
 }
 /**
- * A Cloudpilot event.
+ * A CloudpilotEmu event.
  */
 export interface Event<Payload> {
 	/**
