53c2d38529adc0452720b28f605c9b84257d610b
The Golem <golem@nowhere.org>
2023-01-09 21:45:38 +0000

    Bump.

---
+++ app-preview/5515.796e48dad87ba9e1.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[5515],{5515:(c,s,e)=>{e.r(s),e.d(s,{startStatusTap:()=>a});var l=e(3918),_=e(1559),o=e(3081),r=e(9259);const a=()=>{const n=window;n.addEventListener("statusTap",()=>{(0,_.f)(()=>{const d=document.elementFromPoint(n.innerWidth/2,n.innerHeight/2);if(!d)return;const t=(0,o.f)(d);t&&new Promise(E=>(0,r.c)(t,E)).then(()=>{(0,_.c)((0,l.Z)(function*(){t.style.setProperty("--overflow","hidden"),yield(0,o.s)(t,300),t.style.removeProperty("--overflow")}))})})})}}}]);
+//# sourceMappingURL=5515.796e48dad87ba9e1.js.map
