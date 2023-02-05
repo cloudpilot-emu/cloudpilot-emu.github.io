3c177341a5c940685998a19f73a3a7414ed8569a
The Golem <golem@nowhere.org>
2023-02-05 20:28:48 +0000

    Bump.

---
+++ app-preview/3759.0c92d207bd830f87.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[3759],{3759:(h,s,e)=>{e.r(s),e.d(s,{startStatusTap:()=>a});var l=e(3918),_=e(1559),o=e(4311),r=e(9259);const a=()=>{const n=window;n.addEventListener("statusTap",()=>{(0,_.f)(()=>{const d=document.elementFromPoint(n.innerWidth/2,n.innerHeight/2);if(!d)return;const t=(0,o.f)(d);t&&new Promise(E=>(0,r.c)(t,E)).then(()=>{(0,_.c)((0,l.Z)(function*(){t.style.setProperty("--overflow","hidden"),yield(0,o.s)(t,300),t.style.removeProperty("--overflow")}))})})})}}}]);
+//# sourceMappingURL=3759.0c92d207bd830f87.js.map
